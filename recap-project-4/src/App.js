1a67cc7feb38b554ef2c1e45a7058262520b92f6
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-02-10 16:53:43 +0100

    list

--- recap-project-4/src/App.js
+++ recap-project-4/src/App.js
@@ -1,25 +1,39 @@
 //import logo from "./logo.svg";
 import { uid } from "uid";
 import "./App.css";
-import Form from "./Components/Form";
-import { useState } from "react";
-import List from "./Components/List";
+import Form from "./Components/Form/Form";
+//import { useState } from "react";
+import List from "./Components/List/List";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
 
   function handleAddActivity(newActivity) {
     // console.log(newActivity);
 
-    setActivities([{ newActivity, id: uid }, ...activities]);
+    setActivities([{ ...newActivity, id: uid }, ...activities]);
   }
+
+  const activitiesForGoodWeather = activities.filter(
+    (activity) => activity.isForGoodWeather === true
+  );
+
+  const activitiesForBadWeather = activities.filter(
+    (activity) => activity.isForGoodWeather === false
+  );
 
   console.log(activities);
 
   return (
     <div className="App">
       <h1 className="headline">Show me the weather!</h1>
-      <List activities={activities} />
+      <List
+        activitiesForGoodWeather={activitiesForGoodWeather}
+        activitiesForBadWeather={activitiesForBadWeather}
+      />
       <Form onAddActivity={handleAddActivity} />
     </div>
   );
---
+++ recap-project-4/src/Components/List/List.js
@@ -0,0 +1,27 @@
+import "./List.css";
+
+export default function List(
+  activitiesForGoodWeather,
+  activitiesForBadWeather
+) {
+  console.log(activitiesForBadWeather, activitiesForGoodWeather);
+
+  const whichWeather = activitiesForGoodWeather
+    ? "Weather is fine, let's go outside and eat cake"
+    : "Weather is poor, let's stay inside and eat cake";
+
+  return (
+    <>
+      <div>
+        <h2>{whichWeather}</h2>
+        <ul className="list">
+          {/* {activities.map((activity) => (
+            <li key={activity.id}>
+              <p className="activity-list">{activity.name}</p>
+            </li>
+          ))} */}
+        </ul>
+      </div>
+    </>
+  );
+}
