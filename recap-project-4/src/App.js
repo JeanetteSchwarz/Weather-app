63b15baf0488f95e334c775ea200a2e6b7dd0b8c
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-02-10 17:53:42 +0100

    fetch

--- recap-project-4/src/App.js
+++ recap-project-4/src/App.js
@@ -2,7 +2,7 @@
 import { uid } from "uid";
 import "./App.css";
 import Form from "./Components/Form/Form";
-//import { useState } from "react";
+import { useEffect, useState } from "react";
 import List from "./Components/List/List";
 import useLocalStorageState from "use-local-storage-state";
 
@@ -10,6 +10,9 @@ function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
+  const [currentWeather, setCurrentWeather] = useState();
+  const [temperature, setTemperature] = useState();
+  const [isGoodWeather, setisGoodWeather] = useState();
 
   function handleAddActivity(newActivity) {
     // console.log(newActivity);
@@ -25,11 +28,33 @@ function App() {
     (activity) => activity.isForGoodWeather === false
   );
 
+  const emoji = isGoodWeather ? "☀️" : "🌧️";
+  const currentTemperature = currentWeather.temperature;
+
   console.log(activities);
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch(
+        "https://example-apis.vercel.app/api/weather"
+      );
+      const currentWeather = await response.json();
+      console.log(currentWeather);
+
+      setCurrentWeather(currentWeather);
+      setTemperature(temperature);
+      setisGoodWeather(isGoodWeather);
+    }
+    startFetching();
+  }, []);
 
   return (
     <div className="App">
       <h1 className="headline">Show me the weather!</h1>
+      <p className="display">
+        {emoji}
+        {currentTemperature} °C
+      </p>
       <List
         activitiesForGoodWeather={activitiesForGoodWeather}
         activitiesForBadWeather={activitiesForBadWeather}
